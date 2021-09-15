b80a55fb9444f63c6e43a0c9e401a41a9f07e5ae
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2021-09-15 15:23:21 +0200

    Test: Add a first snapshot test for `drawCanvas`

--- Resources/Private/JavaScript/VideoPlayer/Screenshot.test.js
+++ Resources/Private/JavaScript/VideoPlayer/Screenshot.test.js
@@ -1,5 +1,54 @@
-const { renderScreenshot } = require('./Screenshot');
+/**
+ * @jest-environment jsdom
+ */
 
-test('basic', () => {
-  expect(renderScreenshot).toBeInstanceOf(Function);
+const { drawCanvas } = require('./Screenshot');
+
+class VideoMock extends HTMLVideoElement {
+  constructor(width, height) {
+    super();
+
+    this._mockWidth = width;
+    this._mockHeight = height;
+  }
+
+  get videoWidth() {
+    return this._mockWidth;
+  }
+
+  get videoHeight() {
+    return this._mockHeight;
+  }
+}
+
+customElements.define('video-mock', VideoMock, { extends: 'video' });
+
+test('can draw to canvas', () => {
+  const metadata = {
+    metadata: {
+      title: "Test Video",
+      year: "1912",
+      not_a_string: 123,
+    },
+    screenshotFields: [
+      "title",
+      "year",
+      "not_a_string",
+    ],
+  };
+
+  const snapshotWithSize = (videoWidth, videoHeight) => {
+    const video = new VideoMock(videoWidth, videoHeight);
+
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext('2d');
+
+    drawCanvas(context, video, metadata);
+
+    const events = context.__getEvents();
+    expect(events).toMatchSnapshot();
+  };
+
+  snapshotWithSize(1920, 1080);
+  snapshotWithSize(960, 540);
 });
