fd6dc283161f2f8177093e1a3ade9aa1ebc888a8
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-05-25 09:43:46 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.2.0',
+    'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.5.26-9.5.99',
+            'typo3' => '9.5.0-10.4.99',
         ),
         'conflicts' => array(
         ),
