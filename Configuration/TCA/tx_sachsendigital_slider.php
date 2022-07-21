e6875715841278ebaf6b6fdc8e56d16e69db5592
Thomas Jung <thomas@jung.digital>
2022-07-21 16:29:24 +0200

    Add new content element for homepage slider
    - Add SQL statement to create a new field and corresponding table
    - Add TCA configurations for new table and the tt_content
    - Register new backend icon
    - Add Typoscript configuration and Fluid template

---
+++ Configuration/TCA/tx_sachsendigital_slider.php
@@ -0,0 +1,159 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+return [
+    'ctrl' => [
+        'label' => 'title',
+        'sortby' => 'sorting',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'title' => 'Slider Items',
+        'iconfile' => 'EXT:slub_web_sachsendigital/Resources/Public/Icons/Backend/Slider.svg',
+        'delete' => 'deleted',
+        'versioningWS' => true,
+        'origUid' => 't3_origuid',
+        'hideAtCopy' => FALSE,
+        'prependAtCopy' => '',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'languageField' => 'sys_language_uid',
+        'dividers2tabs' => TRUE,
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+    ],
+    'types' => [
+        '1' => ['showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            title,subtitle,url,image,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:visibility;visibility,
+            --palette--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access;access,
+
+        '],
+    ],
+    'palettes' => [
+        '1' => [
+            'showitem' => ''
+        ],
+        'access' => [
+            'showitem' => '
+                starttime;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:starttime_formlabel,
+                endtime;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:endtime_formlabel,
+            ',
+            'canNotCollapse' => 1
+        ],
+        'visibility' => [
+            'showitem' => '
+                hidden;,
+            ',
+            'canNotCollapse' => 1
+        ],
+    ],
+    'columns' => [
+        'tt_content' => [
+            'exclude' => 0,
+            'label' => 'Sachsen.digital Slider',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'foreign_table' => 'tt_content',
+                'foreign_table_where' => 'AND tt_content.pid=###CURRENT_PID### AND tt_content.CType="tx_sachsendigital_slider"',
+                'maxitems' => 1,
+            ],
+        ],
+        'hidden' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    '1' => [
+                        '0' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:hidden.I.0'
+                    ]
+                ]
+            ]
+        ],
+        'starttime' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+            'config' => [
+                'type' => 'input',
+                'size' => '13',
+                'max' => '20',
+                'eval' => 'datetime',
+                'default' => '0',
+                'renderType' => 'inputDateTime'
+            ],
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly'
+        ],
+        'endtime' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+            'config' => [
+                'type' => 'input',
+                'size' => '13',
+                'max' => '20',
+                'eval' => 'datetime',
+                'default' => '0',
+                'range' => [
+                    'upper' => mktime(0, 0, 0, 12, 31, 2040)
+                ],
+                'renderType' => 'inputDateTime'
+            ],
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly'
+        ],
+        'title' => [
+            'exclude' => 0,
+            'label' => 'Slide Title',
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'eval' => 'trim,required',
+            ],
+        ],
+        'subtitle' => [
+            'exclude' => 0,
+            'label' => 'Subtitle',
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'eval' => 'trim',
+            ],
+        ],
+        'url' => [
+            'exclude' => 0,
+            'label' => 'Slide Link',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputLink',
+            ],
+        ],
+        'image' => [
+            'exclude' => 0,
+            'label' => 'Image',
+            'config' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+                'image',
+                [
+                    'overrideChildTca' => [
+                        'types' => [
+                            \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+                                'showitem' => '--palette--;LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,--palette--;;filePalette'
+                            ],
+                        ]
+                    ],
+                    'minitems' => 0,
+                    'maxitems' => 1,
+                    'fileUploadAllowed' => TRUE,
+                ],
+                $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext']
+            ),
+        ]
+    ]
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -18,3 +18,21 @@ defined('TYPO3_MODE') or die();
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['slub_web_sachsendigital'] = 'EXT:slub_web_sachsendigital/Configuration/RTE/Default.yaml';
+
+/*
+ * Register some backend icons
+ */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$icons = ['Slider'];
+foreach ($icons as $icon) {
+    $iconRegistry->registerIcon(
+        'SachsendigitalWebsite' . $icon,
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:slub_web_sachsendigital/Resources/Public/Icons/Backend/' . $icon . '.svg']
+    );
+}
+
+/*
+ * Allow custom content elements on all standard pages
+ */
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sachsendigital_slider');
